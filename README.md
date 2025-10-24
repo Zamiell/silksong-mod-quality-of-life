1c3942bf18850b35e9124da36a466184e46d12d9
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-10-24 17:15:05 -0400

    feat: config

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ It uses the [BepInExPack for Silksong](https://thunderstore.io/c/hollow-knight-s
 
 ## Features
 
-### Instant Main Menu
+### 1. Instant Main Menu
 
 - Skips the intro sequence (`Pre_Menu_Intro`).
 - Loads the main menu (`Menu_Title`) instantly when you launch the game.
+
+## Configuration
+
+The mod can be configured by editing the configuration file located at: `BepInEx/config/io.github.zamiel.qol.cfg`
--- SilksongModQualityOfLife.cs
+++ SilksongModQualityOfLife.cs
@@ -1,4 +1,5 @@
 using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
@@ -8,8 +9,17 @@ namespace QoL
     [BepInPlugin("io.github.zamiel.qol", "QoL", "0.0.1")]
     public class QoL : BaseUnityPlugin
     {
+        public static ConfigEntry<bool> SkipIntro { get; private set; } = null!;
+
         private void Awake()
         {
+            SkipIntro = Config.Bind(
+                "General",
+                "SkipIntro",
+                true,
+                "Skip the intro sequence and load the main menu directly when launching the game."
+            );
+
             var harmony = new Harmony("io.github.zamiel.qol");
             harmony.PatchAll();
 
@@ -25,6 +35,11 @@ namespace QoL
     {
         static bool Prefix(AddressablesLoadScene __instance)
         {
+            if (!QoL.SkipIntro.Value)
+            {
+                return true;
+            }
+
             // address
             var addressFieldMaybe = typeof(AddressablesLoadScene).GetField(
                 "address",
