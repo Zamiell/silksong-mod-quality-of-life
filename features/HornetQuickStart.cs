fd2862acc690bd0aebae2595935939f0a85bdf14
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-10-29 20:04:26 -0400

    feat: Add Hornet Quick Start option
    Enable skipping the opening credits animation when starting a new game.
    
    (This commit message was automatically generated by an LLM.)

--- features/HornetQuickStart.cs
+++ features/HornetQuickStart.cs
@@ -1,3 +1,18 @@
+using HarmonyLib;
+
 namespace QoL.Features;
 
-public class PlayMakerFSM_SendEvent_Patch { }
+[HarmonyPatch(typeof(GameManager), "StartNewGame")]
+public class GameManager_StartNewGame_Patch
+{
+    static void Postfix()
+    {
+        if (!Config.HornetQuickStart.Value)
+        {
+            return;
+        }
+
+        // Skip the opening credits animation when starting a new game.
+        PlayerData.instance.openingCreditsPlayed = true;
+    }
+}
